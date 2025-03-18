a4e03fdf83f2d8b83157a274461bd112775f998e
delomast <delomast@gmail.com>
2025-03-18 11:54:37 -0400

    better bug fix

--- R/HNC_expand_one_strat.R
+++ R/HNC_expand_one_strat.R
@@ -135,7 +135,7 @@ HNC_expand_one_strat_MLE <- function(trap, H_vars, HNC_vars, W_vars, wc_expanded
 										 !is.na(!!as.symbol(v1))) %>% nrow()
 	tempTrap <- trap %>% filter(LGDMarkAD == "AI", !is.na(!!as.symbol(v1)))
 	# need to skip iter or throw error if there are no HNC fish in tempTrap, but HNC estimate is not 0
-	if(numHNC == 0 && (pPhys > 0 || !all.equal(pPBTonly, 0))){
+	if(numHNC == 0 && (pPhys > 0 || !isTRUE(all.equal(pPBTonly, 0)))){
 		# print("testing HNC NULL")
 		return(NULL)
 	}
@@ -218,7 +218,7 @@ HNC_expand_one_strat_MLE <- function(trap, H_vars, HNC_vars, W_vars, wc_expanded
 							group_by(var1) %>% summarise(total = sum(total), .groups = "drop") %>% select(var1, total) %>%
 							rename(group = var1)
 					) %>% group_by(group) %>% summarise(total = sum(total), .groups = "drop")
-			} else if(is.character(all.equal(((1 - pPhys) * pPBTonly), 0)) || !all.equal(((1 - pPhys) * pPBTonly), 0)){
+			} else if(!isTRUE(all.equal(((1 - pPhys) * pPBTonly), 0))){
 				# missing data issues, skip this bootstrap or throw an error
 				# print("testing HNC v1.3 NULL")
 				return(NULL)
@@ -334,7 +334,7 @@ HNC_expand_one_strat_MLE <- function(trap, H_vars, HNC_vars, W_vars, wc_expanded
 										 	filter(group != "Unassigned") %>% rename(var1 = group) %>%
 										 	select(var1, var2, total)
 				) %>% group_by(var1, var2) %>% summarise(total = sum(total), .groups = "drop")
-			} else if(is.character(all.equal(((1 - pPhys) * pPBTonly), 0)) || !all.equal(((1 - pPhys) * pPBTonly), 0)){
+			} else if(!isTRUE(all.equal(((1 - pPhys) * pPBTonly), 0))){
 				# missing data issues, skip this bootstrap or throw an error
 				# print("testing HNC v1.3 NULL")
 				return(NULL)
@@ -356,7 +356,7 @@ HNC_expand_one_strat_MLE <- function(trap, H_vars, HNC_vars, W_vars, wc_expanded
 	numW <- tempTrap %>% filter(!pbtAssign) %>% nrow()
 
 	# need to skip iter or throw error if there are no W fish in tempTrap, but wild proportion is not 0
-	if(numW == 0 && !all.equal((1 - pClip) * (1 - pPhys) * (1 - pPBTonly), 0)){
+	if(numW == 0 && !isTRUE(all.equal((1 - pClip) * (1 - pPhys) * (1 - pPBTonly), 0))){
 		# print("testing W NULL")
 		return(NULL)
 	}
